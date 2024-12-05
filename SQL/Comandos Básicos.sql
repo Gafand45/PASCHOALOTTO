25b411fb9611c1f265e8f12c081cdd0e3e073fb0
Gafand45 <gaferandj@gmail.com>
2024-12-05 11:38:59 -0300

    Reorganizacao

--- SQL/Comandos Básicos.sql
+++ SQL/Comandos Básicos.sql
@@ -1,6 +1,8 @@
-CREATE TABLE tb_atendimentos(
-ID_Atendimento SERIAL,
-Data_Atendimento DATE,
+CREATE SCHEMA IF NOT EXISTS atendimentos;
+
+CREATE TABLE atendimentos.tb_atendimentos (
+ID_Atendimento SERIAL PRIMARY KEY, -- Identificador único
+Data_Atendimento TIMESTAMP NOT NULL, 
 Tipo_Atendimento VARCHAR(50),
 Status VARCHAR(50),
 Meio_Contato VARCHAR(50),
