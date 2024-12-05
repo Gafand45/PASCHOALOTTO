51b5b7e03692ce8b58ff0c82a138e1d1ab7da8bd
Gafand45 <gaferandj@gmail.com>
2024-12-05 11:09:38 -0300

    SQL Atualização

--- SQL/Comandos Básicos.sql
+++ SQL/Comandos Básicos.sql
@@ -1,2 +1,9 @@
---isso é um teste
-SELECT A from tablesss
+CREATE TABLE tb_atendimentos(
+ID_Atendimento SERIAL,
+Data_Atendimento DATE,
+Tipo_Atendimento VARCHAR(50),
+Status VARCHAR(50),
+Meio_Contato VARCHAR(50),
+Tempo_Resolucao_minutos INTEGER,
+Avalicao INTEGER
+)
